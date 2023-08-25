3a6efd9ab46ef8348e3595ad26429044bf88dcd0
Alexander Fontler <mallexthewise@gmail.com>
2023-08-25 18:51:42 +0200

    Typescript fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import type { AppProps } from "next/app"
 
-export default function App({
-  Component, pageProps: { session, ...pageProps },
-}) {
+// Use the <SessionProvider> to improve performance and allow components that call
+// `useSession()` anywhere in your application to access the `session` object.
+export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider
+      // Provider options are not required but can be useful in situations where
+      // you have a short session maxAge time. Shown here with default values.
+      session={pageProps.session}
+    >
       <Component {...pageProps} />
     </SessionProvider>
   )
